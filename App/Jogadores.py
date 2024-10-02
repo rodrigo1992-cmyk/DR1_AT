0c24408b4297a91d5e963155200bac1d18976caf
rodrigo1992-cmyk <145853715+rodrigo1992-cmyk@users.noreply.github.com>
2024-10-02 01:52:38 -0300

    ok

--- App/Jogadores.py
+++ App/Jogadores.py
@@ -3,11 +3,11 @@ import pandas as pd
 import streamlit as st
 from utils import *
 
-filtro_partida = st.session_state.filtro_partida
-
 
 ######################################  INICIA O APLICATIVO ######################################
 def exibir():
+    filtro_partida = st.session_state.filtro_partida
+    
     loading_bar(filtro_partida)
     if st.session_state.filtro_partida != 'Selecione':
         aba_jogadores(st.session_state.df_eventos)
--- App/Partidas.py
+++ App/Partidas.py
@@ -6,19 +6,23 @@ from utils import *
 ######################################  INICIA O APLICATIVO ######################################
 
 #Carrego as variáveis de sessão em variáveis locais para facilitar na hora de passar como parâmetro das funções
-df_partidas_filtrado = st.session_state.df_partidas_filtrado
-df_eventos = st.session_state.df_eventos
-filtro_camp = st.session_state.filtro_camp
-filtro_ano = st.session_state.filtro_ano
-filtro_partida = st.session_state.filtro_partida
-
-if df_partidas_filtrado.empty:
-    pass
-else:
-    home_team = df_partidas_filtrado.home_team.values[0]
-    away_team  = df_partidas_filtrado.away_team.values[0]
-
 def exibir():
+
+    df_partidas_filtrado = st.session_state.df_partidas_filtrado
+    df_eventos = st.session_state.df_eventos
+    filtro_camp = st.session_state.filtro_camp
+    filtro_ano = st.session_state.filtro_ano
+    filtro_partida = st.session_state.filtro_partida
+
+    if df_partidas_filtrado.empty:
+        pass
+    else:
+        home_team = df_partidas_filtrado.home_team.values[0]
+        away_team  = df_partidas_filtrado.away_team.values[0]
+
+
+    st.write(filtro_partida)
+    st.write(st.session_state.filtro_partida)
     loading_bar(filtro_partida)
     resumo_selecao(df_partidas_filtrado, df_eventos, filtro_camp, filtro_ano, filtro_partida)
     
